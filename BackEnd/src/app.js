e848b0d2efb83faf642b16988029e1ef962dc939
Rafael Bernardes Sposito <rajingg7@gmail.com>
2026-06-12 17:42:35 -0300

    Back-end funcinando novamente

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -7,10 +7,6 @@ const app = express()
 app.use(cors());
 app.use(express.json());
 
-//imports
-import cadastroRoute from "./routes/CadastroRoute.js"
 
-// conecta as rotas do usuário
-app.use(cadastroRoute);
 
 export default app;
